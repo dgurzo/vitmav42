752e770dec3746505314aa63fcd4b9e6cf7f4bce
dgurzo <dgurzo@gmail.com>
2019-12-19 15:40:47 +0100

    html helyett ejs

--- middleware/dal/saveDalMW.js
+++ middleware/dal/saveDalMW.js
@@ -8,6 +8,12 @@ const requireOption = require('../requireOption');
 
 module.exports = function (objectrepository) {
     return function (req, res, next) {
-        next();
+        if(typeof req.body.cim === 'undefined' ||
+            typeof req.body.album === 'undefined' ||
+            typeof req.body.kiadasev === 'undefined') {
+                return next();
+        }
+        console.log(req.body);
+        return res.redirect('/dal/01');
     };
 };
--- middleware/renderMW.js
+++ middleware/renderMW.js
@@ -6,8 +6,6 @@ const requireOption = require('./requireOption');
 
 module.exports = function (objectrepository, viewName) {
     return function (req, res) {
-        //res.render(viewName, res.tpl);
-        console.log('render: ' + viewName);
-        res.end('Template: ' + viewName);
+        res.render(viewName, res.locals);
     };
 };
